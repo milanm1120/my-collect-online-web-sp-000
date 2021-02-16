d086a67850e9c2e73809188fad39dfd27538d05e
Milan Mody <milanm1120@gmail.com>
2021-02-16 01:27:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
   i = 0
   while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  array
+      binding.pry
+      yield(array[i])
+      i += 1
+    end
+    array
 end
